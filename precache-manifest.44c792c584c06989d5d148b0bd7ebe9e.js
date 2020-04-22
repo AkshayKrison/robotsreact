b1cc64a4964a996fbe6d8dfd56be9fc095a1fe41
Akshay Krison <akshaykrisonz@gmail.com>
2020-04-22 03:43:10 -0700

    Updates

---
+++ precache-manifest.44c792c584c06989d5d148b0bd7ebe9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1884551d7e6c762b63d7703da11e881",
+    "url": "/robotsreact/index.html"
+  },
+  {
+    "revision": "7161fea693a5e94594bd",
+    "url": "/robotsreact/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "3b87add6bf79fc1a795f",
+    "url": "/robotsreact/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "7161fea693a5e94594bd",
+    "url": "/robotsreact/static/js/2.c81d8bdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotsreact/static/js/2.c81d8bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b87add6bf79fc1a795f",
+    "url": "/robotsreact/static/js/main.3a721663.chunk.js"
+  },
+  {
+    "revision": "92c998026efd6fdc9a65",
+    "url": "/robotsreact/static/js/runtime-main.22f4581a.js"
+  }
+]);
